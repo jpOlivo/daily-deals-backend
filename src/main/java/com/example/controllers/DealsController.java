fd3f4cc2580b756473d946c83f3f3ca9afcc651b
jo14583 <jo14583@LACARGBALD0666.lac.nsroot.net>
2018-07-17 23:24:16 -0300

    Initial commit

---
+++ src/main/java/com/example/controllers/DealsController.java
@@ -0,0 +1,75 @@
+package com.example.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.models.Deal;
+
+@RestController
+@RequestMapping("/api/deals")
+@CrossOrigin("*")
+public class DealsController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DealsController.class);	
+
+
+	@GetMapping("/public")
+	public List<Deal> getDealsPublic() {
+		LOGGER.info("getDealsPublic");
+		
+		List<Deal> deals = new ArrayList<Deal>();
+		Deal deal = new Deal();
+		deal.setId(1234);
+		deal.setName("Galaxy Note 7");
+		deal.setDescription("The Note 7 has been fixed and will no longer explode. Get it an amazing price!");
+		deal.setOriginalPrice(899.99);
+		deal.setSalePrice(499.99);
+		deals.add(deal);
+
+		deal = new Deal();
+		deal.setId(2016);
+		deal.setName("Macbook Pro 2016");
+		deal.setDescription("The Mac Book Pro is the de-facto standard for best in breed mobile computing.");
+		deal.setOriginalPrice(2199.99);
+		deal.setSalePrice(1999.99);
+		deals.add(deal);
+		
+		deal = new Deal();
+		deal.setId(4444);
+		deal.setName("GoPro 4");
+		deal.setDescription("Record yourself in first person 24/7 with the GoPro 4. Show everyone how exciting your life is.");
+		deal.setOriginalPrice(299.99);
+		deal.setSalePrice(199.99);
+		deals.add(deal);
+		
+		
+		return deals;
+	}
+	
+	@GetMapping("/private")
+	@PreAuthorize("hasAuthority('read:deals')")
+	public List<Deal> getDealsPrivate() {
+		LOGGER.info("getDealsPrivate");
+		
+		Deal deal = new Deal();
+		deal.setId(5678);
+		deal.setName("Amazon Echo");
+		deal.setDescription("Turn your home into smart home with Amazon Echo. Just say the word and Echo will do it.");
+		deal.setOriginalPrice(179.99);
+		deal.setSalePrice(129.99);
+		
+		List<Deal> deals = new ArrayList<Deal>();
+		deals.add(deal);
+		
+		return deals;
+	}
+
+}
---
+++ src/main/java/com/example/models/Deal.java
@@ -0,0 +1,49 @@
+package com.example.models;
+
+public class Deal {
+	private Integer id;
+	private String name;
+	private String description;
+	private Double originalPrice;
+	private Double salePrice;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getOriginalPrice() {
+		return originalPrice;
+	}
+
+	public void setOriginalPrice(Double originalPrice) {
+		this.originalPrice = originalPrice;
+	}
+
+	public Double getSalePrice() {
+		return salePrice;
+	}
+
+	public void setSalePrice(Double salePrice) {
+		this.salePrice = salePrice;
+	}
+}
